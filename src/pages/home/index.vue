d1d86819324c8ce5878d3084faefe630c28d0512
kli59 <kli59@mail.yst.com.cn>
2026-03-25 22:34:46 +0800

    feat: 项目初始化

---
+++ src/pages/home/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <view class="container">
+        <scroll-view :show-scrollbar="false" :enhanced="true" class="scroll-view" scroll-y>
+            <view v-for="item in 200" :key="item" class="item">
+                <text>{{ item }}</text>
+            </view>
+        </scroll-view>
+        <view>
+            <text>1111</text>
+        </view>
+    </view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+page {
+    height: 100vh;
+}
+
+.container {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+
+.scroll-view {
+    flex: 1;
+    overflow: hidden;
+}
+</style>
